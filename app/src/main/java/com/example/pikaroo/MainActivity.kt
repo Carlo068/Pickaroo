9c1423185a8ec11d3f9d155c89bd23d24b8fac4f
Justin Salim <justin@MacBook-Air-de-Justin.local>
2026-03-23 19:35:58 -0600

    ¨hola¨

--- app/src/main/java/com/example/pikaroo/MainActivity.kt
+++ app/src/main/java/com/example/pikaroo/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     PikarooTheme {
-        Greeting("Android")
+        Greeting("Carlo y Justin")
     }
 }
